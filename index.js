0863340ad6b4094369e2f38ea0d6d78a33dbabcb
Falon Darville <falondarville@gmail.com>
2018-04-05 15:24:55 -0700

    add instructions as comments

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
 var wordBank = ["asparagus", "carrots", "spinach", "lettuce"];
+var randomWord = Math.floor(Math.random() * wordBank.length);
+var randomArray = wordBank[randomWord];
